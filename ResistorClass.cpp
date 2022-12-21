2fc569c68c34e97d42b3fa272eceb240cfb3834b
Juan Medina <121105074+JuanManuelMedinaSanchez@users.noreply.github.com>
2022-12-20 21:58:20 -0500

    Update ResistorClass.cpp

--- ResistorClass.cpp
+++ ResistorClass.cpp
@@ -1,88 +1,89 @@
 /*
     WDFComponents: TwoPortResistorUnitElement   
 -------------
-    Wave Digital electric components, based in the theory of A. Feitweiss and other source like DAFX Knowledge Created By Juan Manuel Medina for own proposes and Prismatik LAB division: DSP.Erience.**/ This Class is oriented to linear 2PN (Two Port Networks), for implementation / feedback or more info,  write to: prismatiklab@gmail.com
+    Wave Digital electric components, based in the theory of A. Feitweiss and other source like DAFX Knowledge 
+    Created By Juan Manuel Medina for own proposes and Prismatik LAB division: DSP.Erience. This Class is oriented 
+    to linear 2PN (Two Port Networks), for implementation / feedback or more info,  write to: prismatiklab@gmail.com
 -------------
-This class is a model for one resistor with the most posible WDF elements like parasites, reflection, propagation, Transfer function, Z, Y and others parameters. The non-linear coefficient is 0, because the nature of behavior of resistors, capacitors and inductors (linear components)
+    This class is a model for one resistor with the most posible WDF elements like parasites, reflection, propagation, 
+    Transfer function, Z, Y and others parameters. The non-linear coefficient is 0, because the nature of behavior of resistors, 
+    capacitors and inductors (linear components). 
  */
 
 
 class TwoPortResistorUnitElement {
 public:
-  TwoPortResistorUnitElement(double resistance, int numResistors, bool isParallel)
-    : R(resistance), numResistors(numResistors), isParallel(isParallel), Lp(0), Cp(0) {}
+  // Constructor
+  TwoPortResistorUnitElement(double resistance) : R(resistance), Lp(0), Cp(0) {}
+
+  // Set the transfer function
   void setTransferFunction(std::complex<double> H) {
     this->H = H;
   }
-  //parasites
+
+  // Set the parasitic inductance
   void setParasiticInductance(double Lp) {
     this->Lp = Lp;
   }
+
+  // Set the parasitic capacitance
   void setParasiticCapacitance(double Cp) {
     this->Cp = Cp;
   }
 
-  //Z
+  // Set the impedance
   void setImpedance() {
-    if (isParallel) {
-      Z = numResistors / R + Lp; // Take into account the parasitic inductance
-    } else {
-      Z = numResistors * R + Lp; // Take into account the parasitic inductance
-    }
+    Z = 2 * R + Lp; // Take into account the parasitic inductance
   }
 
-  //Admittance Y
+  // Set the admittance
   void setAdmittance() {
-      if (isParallel) {
-      Y = numResistors / (R + 1 / Cp); // Take into account the parasitic capacitance
-    } else {
-      Y = numResistors / (numResistors * R + 1 / Cp); // Take into account the parasitic capacitance
-    }
+    Y = 1 / (2 * R + 1 / Cp); // Take into account the parasitic capacitance
   }
 
-  //Scattering
+  // Set the waveform scattering function
   void setWaveformScatteringFunction(std::array<std::complex<double>, 4> k) {
     this->k = k;
   }
 
-  //nonlinearity coefficient
+  // Set the nonlinearity coefficient
   void setNonlinearityCoefficient(double nl) {
     this->nl = nl;
   }
-  // K Matrix
+
+  // Calculate the input and output waveforms
   void calcInputOutputWaveforms(std::complex<double> V2, std::complex<double> I2) {
     v1 = k[0] * V2 - k[1] * I2;
     i1 = k[2] * V2 - k[3] * I2;
     v2 = v1;
-    i2 = (v2 - v1) / Z;
+    i2 = (v2 - v1) / (2 * R);
   }
 
-//Steady state
+  // Calculate the steady-state waves, scattering and reflection functions, and transmittance
   void calcSteadyStateQuantities() {
-    A = V1 + Z * I1;
-    B = V2 - Z * I2;
+    A = V1 + (2 * R) * I1;
+    B = V2 - (2 * R) * I2;
     a = (A + B) / 2;
-    b = (A - B) / 2;
+    b = (A - B) / 2;  
     K = {a, b, b, a};
     psi = (z - 1) / (z + 1);
   }
 private:
-  double R; 
-  int numResistors; 
-  bool isParallel; 
+  double R;
+  double Lp;
   double Cp; 
   std::complex<double> H; 
-  double Z; 
-  double Y; 
+  double Z; // 
+  double Y; //
   std::array<std::complex<double>, 4> k; 
   double nl; 
   std::complex<double> v1, v2; 
-  std::complex<double> i1, i2;
-  std::complex<double> A, B; //waves
-  std::complex<double> a, b; // Reflections and Scattering 
-  std::array<std::complex<double>, 4> K; // K matrix
-  std::array<std::complex<double>, 4> K; // K matrix
-  std::complex<double> psi; // Psi
-  std::complex<double> V1, V2; // I/O Voltages
-  std::complex<double> I1, I2; // I/O currents 
+  std::complex<double> i1, i2; 
+  std::complex<double> A, B; 
+  std::complex<double> a, b; 
+  std::array<std::complex<double>, 4> K; 
+  std::complex<double> psi; 
+  std::complex<double> z; 
+  std::complex<double> V1, I1; 
+  std::complex<double> V2, I2; // Vout, Iout
 };
